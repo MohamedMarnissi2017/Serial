e0ef5e05a210f08e77ec5640c39bd3db1b1234da
MohamedMarnissi2017 <melmarnissi19962000@gmail.com>
2017-02-27 20:27:20 +0100

    +comentaris

--- NumberComunication1/NumberComunication1.ino
+++ NumberComunication1/NumberComunication1.ino
@@ -12,8 +12,8 @@
 //*********************** VARIABLES *****************************
 
 
-float r1, r2;             // variables for the R's
-float rSerie, rParalel;   // variables for the results
+float r1, r2;             // estableix les variables r1 i r2 amb un valor float
+float rSerie, rParallel;   // estableix les variables rSerie i rParallel amb un valor float
 
 //************************** SETUP ******************************
 
@@ -21,34 +21,35 @@ void setup()
 {
   Serial.begin(9600);// Inicia comunicacio serie a velocitat 9600
   Serial.println("Entra el valor de r1 i r2 (separats per una coma)");//Imprimeix a monitor serie la frase "Entra el valor de r1 i r2 (separats per una coma)"
-  Serial.println();
+  Serial.println();//obre linia nova
 
 }
 
 //*************************** LOOP ******************************
 
 void loop() {
-  while (Serial.available() > 0) {  // if there's any serial available, read it
-    r1 = Serial.parseInt();// look for valid int the incoming serial stream
-    Serial.print("r1=");
-    Serial.print(r1);
-    Serial.print("ohms   ");
-    r2 = Serial.parseInt();
-    Serial.print("r2=");
-    Serial.print(r2);
-    Serial.println("ohms   ");
-    Serial.println();
-    float rSerie = r1 + r2;
-    float rParallel = 1 / (r1 + r2);
-    if (Serial.read() == '\n')
-      Serial.print("rSerie=");
-    Serial.print(rSerie);
-    Serial.print("ohms   ");
-    Serial.print("rParallel=");
-    Serial.print(rParallel);
-    Serial.println("ohms   ");
-    Serial.println();
-    Serial.print("Entra el valor de r1 i r2 (separats per una coma)");//Imprimeix a monitor serie la frase "Entra el valor de r1 i r2 (separats per una coma)"
+  while (Serial.available() > 0) {  //inicia funcio while amb la condiciono seguent: si hi ha algun missatge al canal serei el llegeix, si aquest es superior a 0 es compleix la condicio
+    r1 = Serial.parseInt();// La variable r1 sera igual al missatge en valor int que arribi per el canal serie
+    Serial.print("r1=");//Imprimeix a monitor serie la frase "r1="
+    Serial.print(r1);//Imprimeix a monitor serie el valor de r1
+    Serial.print("ohms   ");//Imprimeix a monitor serie la frase "ohms  "
+    r2 = Serial.parseInt();// La variable r2 sera igual al seguent missatge en valor int que arribi per el canal serie
+    Serial.print("r2=");//Imprimeix a monitor serie la frase "r2="
+    Serial.print(r2);//Imprimeix a monitor serie el valor de r2
+    Serial.println("ohms   ");//Imprimeix a monitor serie la frase "ohms  "
+    Serial.println();//obre linia nova
+    rSerie = r1 + r2;// Suma r1+r2
+    rParallel = 1 / (r1 + r2);// divideix 1 per la suma de r1 i r2
+    if (Serial.read() == '\n')//llegeix linia nova
+      Serial.print("rSerie=");//Imprimeix a monitor serie la frase "rSerie="
+    Serial.print(rSerie);//Imprimeix a monitor serie el valor de la variable rSerie
+    Serial.print("ohms   ");//Imprimeix a monitor serie la frase "ohms  "
+    Serial.print("rParallel=");//Imprimeix a monitor serie la frase "rParallel="
+    Serial.print(rParallel);//Imprimeix a monitor serie el valor de la variable rParallel
+    Serial.println("ohms   ");//Imprimeix a monitor serie la frase "ohms  "
+    Serial.println();;//obre linia nova
+    Serial.println("Entra el valor de r1 i r2 (separats per una coma)");//Imprimeix a monitor serie la frase "Entra el valor de r1 i r2 (separats per una coma)"
+    Serial.println();;//obre linia nova
   }
 }
 
